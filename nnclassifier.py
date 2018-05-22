4c701570a0a0fe36463f353ace37b247b7664946
unknown <ebenz99@gmail.com>
2018-05-22 12:38:27 -0400

    It seems to work but I'm skeptical

--- nnclassifier.py
+++ nnclassifier.py
@@ -18,17 +18,22 @@ class pipeDream:
 		self.ims = arrayOfImages
 		self.labs = arrayOfLabels
 	def findClosestMatch(self, image):
-		dif = 10000
-		label = "default"
+		dif = 1000000
+		label = 10
 		bestIdx = 0
 		for x in range(len(self.labs)):
 			im = self.ims[x]
 			lab = self.labs[x]
 			newDif = np.ndarray.sum(np.subtract(im,image))
+			#print("The new dif is")
+			#print(newDif)
 			if newDif < dif:
 				dif = newDif
 				label = lab
 				bestIdx = x
+		#plt.imshow(self.ims[bestIdx])
+		#plt.title(label)
+		#plt.show()
 		return label
 
 
@@ -57,33 +62,38 @@ TrainSet = pipeDream(ims1,labs1)
 test_data = unpickle(currDir + "\\cifar-10-batches-py\\data_batch_2")
 
 #turns images from batch 2 into np array of train images
-picArr2 = train_data[b'data']
+picArr2 = test_data[b'data']
 npPicArr2 = np.asarray(picArr2, dtype=np.float32)
-ims2 = npPicArr1.reshape(10000, 3, 32, 32).transpose(0,2,3,1).astype("uint8")
+ims2 = npPicArr2.reshape(10000, 3, 32, 32).transpose(0,2,3,1).astype("uint8")
 
 #numeric labels from batch 1 categorization
-labArr2 = train_data[b'labels']
+labArr2 = test_data[b'labels']
 npLabArr2 = np.asarray(labArr2)
 labs2 = npLabArr2
 
 numRight = 0
 newLabels = []
-numToCheck = 100
+numToRead = 300
 
-for num in range(0,numToCheck):
+for num in range(0,len(labs2)):
 	newLab = TrainSet.findClosestMatch(ims2[num])
+	#print(labs2[num])
+	#print(newLab)
+	#plt.imshow(ims2[num])
+	#plt.title(newLab)
+	#plt.show()
 	newLabels.append(newLab)
 	if newLab == labs2[num]:
 		numRight += 1
-	if num > 100:
+	if num > numToRead:
 		break
 
 
-accuracy = numRight / numToCheck
+#print(numRight)
+#print(numToRead)
+
+accuracy = numRight / numToRead
 print(accuracy)
-
-
-
 
 '''
 #idx = random.randint(0,10000)
@@ -92,7 +102,6 @@ print(ims[idx].shape)
 plt.imshow(ims[idx], interpolation="nearest")
 plt.title((meta[b'label_names'])[cats[idx]].decode('utf-8'))
 plt.show()
-
 plt.imshow(ims[idx], interpolation="nearest")
 plt.title((meta[b'label_names'])[testedLab].decode('utf-8'))
 plt.show()
