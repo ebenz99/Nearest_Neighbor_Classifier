04a6ed985f86bc259ca5974148e3c678568f5dca
unknown <ebenz99@gmail.com>
2018-05-21 21:53:55 -0400

    classifier working but doing terrible job

--- nnclassifier.py
+++ nnclassifier.py
@@ -30,26 +30,58 @@ class pipeDream:
 		return label
 
 
+'''TRAINING'''
 #unpacks the CIFAR data
 currDir = os.getcwd()
 train_data = unpickle(currDir + "\\cifar-10-batches-py\\data_batch_1")
-test_data = unpickle(currDir + "\\cifar-10-batches-py\\data_batch_2")
 meta = unpickle(currDir + "\\cifar-10-batches-py\\batches.meta")
 
-
 #turns images from batch 1 into np array of train images
-firstPicArr = train_data[b'data']
-npPicArr = np.asarray(firstPicArr, dtype=np.float32)
-ims = npPicArr.reshape(10000, 3, 32, 32).transpose(0,2,3,1).astype("uint8")
+picArr1 = train_data[b'data']
+npPicArr1 = np.asarray(picArr1, dtype=np.float32)
+ims1 = npPicArr1.reshape(10000, 3, 32, 32).transpose(0,2,3,1).astype("uint8")
+
+#numeric labels from batch 1 categorization
+labArr1 = train_data[b'labels']
+npLabArr1 = np.asarray(labArr1)
+labs1 = npLabArr1
+
+#create object from images and associated labels
+TrainSet = pipeDream(ims1,labs1)
 
 
-firstCatArr = train_data[b'labels']
-npCatArr = np.asarray(firstCatArr)
-cats = npCatArr
+''' TESTING '''
 
-TrainSet = pipeDream(ims,cats)
-testedLab = TrainSet.findClosestMatch(ims[0])
+#unpickles test data from batch 2
+test_data = unpickle(currDir + "\\cifar-10-batches-py\\data_batch_2")
 
+#turns images from batch 2 into np array of train images
+picArr2 = train_data[b'data']
+npPicArr2 = np.asarray(picArr2, dtype=np.float32)
+ims2 = npPicArr1.reshape(10000, 3, 32, 32).transpose(0,2,3,1).astype("uint8")
+
+#numeric labels from batch 1 categorization
+labArr2 = train_data[b'labels']
+npLabArr2 = np.asarray(labArr2)
+labs2 = npLabArr2
+
+numRight = 0
+newLabels = []
+
+for num in range(0,100):
+	newLab = TrainSet.findClosestMatch(ims2[num])
+	newLabels.append(newLab)
+	if newLab == labs2[num]:
+		numRight += 1
+	if num > 100:
+		break
+	#plt.imshow(ims2[num], interpolation="nearest")
+	#plt.title((meta[b'label_names'])[labs2[num]].decode('utf-8'))
+	#plt.show()
+accuracy = numRight / len(labs2)
+print(accuracy)
+
+'''
 #idx = random.randint(0,10000)
 idx = 0
 print(ims[idx].shape)
@@ -60,3 +92,4 @@ plt.show()
 plt.imshow(ims[idx], interpolation="nearest")
 plt.title((meta[b'label_names'])[testedLab].decode('utf-8'))
 plt.show()
+'''
