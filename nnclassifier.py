eda1fdfed2b0dd89c54d85cccab323851c9152ae
unknown <ebenz99@gmail.com>
2018-05-22 13:06:10 -0400

    Works with a little over double the precision of random guessing

--- nnclassifier.py
+++ nnclassifier.py
@@ -25,20 +25,15 @@ class pipeDream:
 			im = self.ims[x]
 			lab = self.labs[x]
 			newDif = np.ndarray.sum(np.subtract(im,image))
-			#print("The new dif is")
-			#print(newDif)
 			if newDif < dif:
 				dif = newDif
 				label = lab
 				bestIdx = x
-		#plt.imshow(self.ims[bestIdx])
-		#plt.title(label)
-		#plt.show()
 		return label
 
 
 '''TRAINING'''
-#unpacks the CIFAR data
+#unpacks the CIFAR data for training set
 currDir = os.getcwd()
 train_data = unpickle(currDir + "\\cifar-10-batches-py\\data_batch_1")
 meta = unpickle(currDir + "\\cifar-10-batches-py\\batches.meta")
@@ -48,7 +43,7 @@ picArr1 = train_data[b'data']
 npPicArr1 = np.asarray(picArr1, dtype=np.float32)
 ims1 = npPicArr1.reshape(10000, 3, 32, 32).transpose(0,2,3,1).astype("uint8")
 
-#numeric labels from batch 1 categorization
+#creates parallel array of numeric labels from batch 1
 labArr1 = train_data[b'labels']
 npLabArr1 = np.asarray(labArr1)
 labs1 = npLabArr1
@@ -66,43 +61,26 @@ picArr2 = test_data[b'data']
 npPicArr2 = np.asarray(picArr2, dtype=np.float32)
 ims2 = npPicArr2.reshape(10000, 3, 32, 32).transpose(0,2,3,1).astype("uint8")
 
-#numeric labels from batch 1 categorization
+#makes parallel numeric labels from batch 2 categorization
 labArr2 = test_data[b'labels']
 npLabArr2 = np.asarray(labArr2)
 labs2 = npLabArr2
 
+#for procedure
 numRight = 0
 newLabels = []
 numToRead = 300
 
+#runs nearest neighbor classification, compares resulting label to real label
 for num in range(0,len(labs2)):
 	newLab = TrainSet.findClosestMatch(ims2[num])
-	#print(labs2[num])
-	#print(newLab)
-	#plt.imshow(ims2[num])
-	#plt.title(newLab)
-	#plt.show()
 	newLabels.append(newLab)
 	if newLab == labs2[num]:
 		numRight += 1
 	if num > numToRead:
 		break
 
-
-#print(numRight)
-#print(numToRead)
-
+#displays accuracy
 accuracy = numRight / numToRead
 print(accuracy)
 
-'''
-#idx = random.randint(0,10000)
-idx = 0
-print(ims[idx].shape)
-plt.imshow(ims[idx], interpolation="nearest")
-plt.title((meta[b'label_names'])[cats[idx]].decode('utf-8'))
-plt.show()
-plt.imshow(ims[idx], interpolation="nearest")
-plt.title((meta[b'label_names'])[testedLab].decode('utf-8'))
-plt.show()
-'''
